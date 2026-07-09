0569017137fa650fffbb9439d758d2087548854d
quickdevelopement <quickdeveloperariful@gmail.com>
2026-07-10 00:28:17 +0600

    Create React App for todo list

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App(){
+  const [task, setTask] = useState("")
+  const [todos, setTodos] = useState([]);
+
+  return <div></div>
+}
+export default App;
